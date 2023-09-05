ca20266eaba08bbf1cd31eb6844744563051fb94
Lucas-FullStackX <lucasleguizamo21@gmail.com>
2023-09-04 22:29:02 -0500

    style: :lipstick: Add Buttons style

--- src/stories/Button.svelte
+++ src/stories/Button.svelte
@@ -4,15 +4,20 @@
 	export let label = '';
 
 	const BASE_BUTTON_CLASS =
-		'cursor-pointer rounded-full border-2 leading-none inline-block font-spaceGrotesk';
+		'cursor-pointer rounded-md leading-none inline-block font-spaceGrotesk px-0.5 py-0.5';
 
-	$: primaryClass = primary
-		? 'text-white bg-pink-600 border-pink-600 dark:bg-pink-700 dark:border-pink-700 hover:bg-pink-500 dark:hover:bg-pink-600'
-		: 'text-neutral-600 bg-transparent border-neutral-600 dark:text-neutral-200 dark:border-neutral-200 hover:bg-pink-200 dark:hover:bg-pink-900';
+	$: primaryButton = primary
+		? 'text-white bg-green hover:bg-green-500'
+		: 'text-black bg-gradient-to-b from-[#FEB751] via-orange to-red-500';
+	$: primaryLabel = primary
+		? 'text-white bg-green hover:bg-green-500'
+		: 'bg-white text-black hover:bg-transparent hover:text-white';
 
-	$: sizeClass = size === 'small' ? 'px-4 py-2.5' : size === 'large' ? 'px-6 py-3' : 'px-5 py-2.5';
+	$: sizeClass = size === 'small' ? 'px-3 py-2' : size === 'large' ? 'px-5 py-2' : 'px-4 py-2';
 </script>
 
-<button type="button" class="{BASE_BUTTON_CLASS} {sizeClass} {primaryClass}" on:click>
-	{label}
+<button type="button" class="{BASE_BUTTON_CLASS} {primaryButton}" on:click>
+	<span class="{sizeClass} {primaryLabel} flex w-full rounded">
+		{label}
+	</span>
 </button>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,29 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const colors = require('tailwindcss/colors');
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
 		extend: {
+			colors: {
+				transparent: 'transparent',
+				current: 'currentColor',
+				black: colors.black,
+				white: colors.white,
+				gray: colors.gray,
+				emerald: colors.emerald,
+				indigo: colors.indigo,
+				yellow: colors.yellow,
+				red: colors.red,
+				orange: {
+					DEFAULT: '#FE6D38',
+					...colors.orange
+				},
+				green: {
+					DEFAULT: '#00C673',
+					...colors.green
+				}
+			},
 			fontFamily: {
 				spaceGrotesk: ['Space Grotesk', 'sans-serif'],
 				['spaceGrotesk-bold']: ['Space Grotesk Bold']
